be874597bc74ec45e48133a1a8f768a85c0bf430
duo8834 <muyu00@qq.com>
2017-07-03 14:01:48 +0800

    第四次

--- todo/models/todo.py
+++ todo/models/todo.py
@@ -2,16 +2,7 @@ import time
 from models import Model
 
 
-# 针对我们的数据 TODO
-# 我们要做 4 件事情
-"""
-C create 创建数据
-R read 读取数据
-U update 更新数据
-D delete 删除数据
 
-Todo.new() 来创建一个 todo
-"""
 class Todo(Model):
     @classmethod
     def new(cls, form):
@@ -34,7 +25,7 @@ class Todo(Model):
             'completed'
         ]
         for key in form:
-            # 这里只应该更新我们想要更新的东西
+            
             if key in valid_names:
                 setattr(t, key, form[key])
         t.save()
@@ -42,11 +33,7 @@ class Todo(Model):
 
     @classmethod
     def complete(cls, id, completed=True):
-        """
-        用法很方便
-        Todo.complete(1)
-        Todo.complete(2, False)
-        """
+       
         t = cls.find(id)
         t.completed = completed
         t.save()
@@ -55,9 +42,6 @@ class Todo(Model):
     def __init__(self, form):
         self.id = None
         self.title = form.get('title', '')
-        # 下面的是默认的数据
         self.completed = False
-        # ct ut 分别是 created_time  updated_time
-        # 创建时间和 更新时间
         self.ct = int(time.time())
         self.ut = self.ct
